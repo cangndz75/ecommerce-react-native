47b35d751febca1a55e04cd58aec52a2e20d151e
Can Gündüz <cangunduz0001@gmail.com>
2024-09-12 01:49:31 +0300

    Startup files & mongodb | 1.0.1

---
+++ app.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import Fastify from "fastify";
+import { connectDB } from "./src/config/connect.js";
+
+const start = async () => {
+  await connectDB(process.env.MONGO_URI);
+  const app = Fastify();
+  const PORT = process.env.PORT || 3000;
+  app.listen({ port: PORT, host: "0.0.0.0" }, (err, addr) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Server listening at ${PORT}`);
+    }
+  });
+};
+
+
+start();
